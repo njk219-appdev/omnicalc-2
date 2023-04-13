08dbba6de0862d3defcd8a0f76333147fe1611fc
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-13 15:37:55 +0000

    all math complete!

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -21,4 +21,26 @@ class MathController < ApplicationController
 
     render({:template => "math_templates/subtraction_form.html.erb"})
   end
+  def multiply_results
+    #params = {"first_num" => }
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @second * @first
+    render({:template => "math_templates/multiply_results.html.erb"})
+  end
+  def multiply_form
+
+    render({:template => "math_templates/multiply_form.html.erb"})
+  end
+  def divide_results
+    #params = {"first_num" => }
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first / @second
+    render({:template => "math_templates/divide_results.html.erb"})
+  end
+  def divide_form
+
+    render({:template => "math_templates/divide_form.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get("/add", {:controller => "math", :action => "add_form"})
   get("/wizard_subtract", {:controller => "math", :action => "subtract_results"})
   get("/subtract", {:controller => "math", :action => "subtract_form"})
+  get("/wizard_multiply", {:controller => "math", :action => "multiply_results"})
+  get("/multiply", {:controller => "math", :action => "multiply_form"})
+  get("/wizard_divide", {:controller => "math", :action => "divide_results"})
+  get("/divide", {:controller => "math", :action => "divide_form"})
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
